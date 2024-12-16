ce241f81b6378de7cd44840a7f19f49f0d686cf1
aliAkkayaJ <aliakkayaa0@gmail.com>
2024-12-16 21:30:26 +0300

    Add: Comment get and include. Install: newtonsoft

--- api/Repository/CommentRepository.cs
+++ api/Repository/CommentRepository.cs
@@ -16,9 +16,20 @@ namespace api.Repository
         {
             _context = context;
         }
+
+        public Task<Comment> CreateAsync(Comment commentModel)
+        {
+            throw new NotImplementedException();
+        }
+
         public async Task<List<Comment>> GetAllAsync()
         {
             return await _context.Comments.ToListAsync();
+        }
+
+        public async Task<Comment?> GetByIdAsync(int id)
+        {
+            return await _context.Comments.FindAsync(id);
         }
     }
 }
