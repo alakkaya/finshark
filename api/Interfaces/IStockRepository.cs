d34ab1c42e226d53c597b0caa400d889ae79fcd0
Ali Akkaya <117384310+alakkaya@users.noreply.github.com>
2024-12-27 18:52:31 +0300

    add: filtering

--- api/Interfaces/IStockRepository.cs
+++ api/Interfaces/IStockRepository.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Dtos.Stock;
+using api.Helpers;
 using api.Models;
 
 namespace api.Interfaces
 {
     public interface IStockRepository
     {
-        Task<List<Stock>> GetAllAsync();
+        Task<List<Stock>> GetAllAsync(QueryObject query);
         Task<Stock?> GetByIdAsync(int id); //firstOrDefault can be null what why "?"
         Task<Stock> CreateAsync(Stock stockModel);// can't be null because of that we don't use "?"
         Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
