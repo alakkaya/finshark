3d2d34619f4b632774bb84c52b77496de854e875
Ali Akkaya <117384310+alakkaya@users.noreply.github.com>
2025-01-31 16:41:00 +0300

    add:  user registration with role assignment

---
+++ api/Migrations/20250131133602_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "6e7a5e32-3381-4908-b1aa-744305042d62", null, "Admin", "ADMIN" },
+                    { "92177697-1222-447a-a212-4ff4b36c61ad", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6e7a5e32-3381-4908-b1aa-744305042d62");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "92177697-1222-447a-a212-4ff4b36c61ad");
+        }
+    }
+}
